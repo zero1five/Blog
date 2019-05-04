096ea73b4685ca9e6e99069d3f60493ea7bc28d5
zero1five <zerodengyin@gmail.com>
2019-05-04 11:26:51 +0800

    feat: update frontend code | 更新 client code

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import { init } from 'react-crx';
+
+const { Put, Ctx } = init({foo: 1});
+
+function App() {
+  return (
+    <div className="App">
+      <Ctx>{s => <div>{s.foo}</div>}</Ctx>
+      <button onClick={() => Put(s => (s.foo = s.foo + 1))}>add</button>
+    </div>
+  );
+}
+
+export default App;
