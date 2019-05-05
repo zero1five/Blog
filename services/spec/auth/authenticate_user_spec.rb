4543f6509cf0014d040d948dd4bdf00d79834dab
zero1five <zerodengyin@gmail.com>
2019-05-05 15:58:05 +0800

    feat: Perfecting login / sign logic | 完善 登录 / 注册

---
+++ services/spec/auth/authenticate_user_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe AuthenticateUser do
+  # 创建一个测试用户
+  let(:user) { create(:user) }
+  # 有效的请求
+  subject(:valid_auth_obj) { described_class.new(user.email, user.password) }
+  # 无效的请求
+  subject(:invalid_auth_obj) { described_class.new('foo', 'bar') }
+
+  describe '#call' do
+    context 'when valid credentials' do
+      it 'returns an auth token' do
+        token = valid_auth_obj.call
+        expect(token).not_to be_nil  
+      end
+    end
+
+    context 'when invalid credentials' do
+      it 'raises an authentication error' do
+        expect { invalid_auth_obj.call }
+          .to raise_error(
+            ExceptionHandler::AuthenticationError,
+            /Invalid credentials/
+          )
+      end
+    end
+  end
+end
